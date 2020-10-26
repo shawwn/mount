61aad4e79e27551d96517ebeabcbb34a6da24405
Shawn Presser <shawnpresser@gmail.com>
2020-10-26 23:42:35 +0000

    Ensure botocore is up to date

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 pip3 install git+git://github.com/shawwn/ratarmount.git
 pip3 install git+git://github.com/shawwn/simple-httpfs.git
+
+# an old botocore has caused trouble on a few servers, so let's just
+# upgrade it and call it even.
+pip3 install -U botocore
