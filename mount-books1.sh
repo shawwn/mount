f9994d0a9f49fe5950a8bceb5f71ba0d2c0f23cc
Shawn Presser <shawnpresser@gmail.com>
2020-10-26 16:45:10 -0500

    Simplify scripts

--- mount-books1.sh
+++ mount-books1.sh
@@ -1,11 +1,11 @@
-umount httproot
-umount httpsroot
+umount http
+umount https
 umount books1
-mkdir -p httproot
-mkdir -p httpsroot
+mkdir -p http
+mkdir -p https
 mkdir -p books1
 set -ex
-python3 -m simple_httpfs --schema http httproot
-python3 -m simple_httpfs --schema https httpsroot
+python3 -m simple_httpfs http
+python3 -m simple_httpfs https
 [ ! -f books1.tar.gz.index.sqlite ] && wget https://battle.shawwn.com/books1.tar.gz.index.sqlite
-ratarmount httpsroot/the-eye.eu/public/AI/pile_preliminary_components/books1.tar.gz.. books1 --index-file books1.tar.gz.index.sqlite
+ratarmount https/the-eye.eu/public/AI/pile_preliminary_components/books1.tar.gz.. books1 --index-file books1.tar.gz.index.sqlite
--- mount-books3.sh
+++ mount-books3.sh
@@ -1,11 +1,11 @@
-umount httproot
-umount httpsroot
+umount http
+umount https
 umount books3
-mkdir -p httproot
-mkdir -p httpsroot
+mkdir -p http
+mkdir -p https
 mkdir -p books3
 set -ex
-python3 -m simple_httpfs --schema http httproot
-python3 -m simple_httpfs --schema https httpsroot
+python3 -m simple_httpfs http
+python3 -m simple_httpfs https
 [ ! -f books3.tar.gz.index.sqlite ] && wget https://battle.shawwn.com/books3.tar.gz.index.sqlite
-ratarmount httpsroot/the-eye.eu/public/AI/pile_preliminary_components/books3.tar.gz.. books3 --index-file books3.tar.gz.index.sqlite
+ratarmount https/the-eye.eu/public/AI/pile_preliminary_components/books3.tar.gz.. books3 --index-file books3.tar.gz.index.sqlite
