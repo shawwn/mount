87f1dda79249643db2b8ecf29d546cfafc2f5506
Shawn Presser <shawnpresser@gmail.com>
2023-01-30 07:05:32 -0600

    Fix github urls

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-pip3 install git+git://github.com/shawwn/ratarmount.git
-pip3 install git+git://github.com/shawwn/simple-httpfs.git
+set -ex
+
+pip3 install git+https://github.com/shawwn/ratarmount.git
+pip3 install git+https://github.com/shawwn/simple-httpfs.git
 
 # an old botocore has caused trouble on a few servers, so let's just
 # upgrade it and call it even.
